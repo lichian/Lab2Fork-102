1189ce7e79022edd815b63d7c7d4eb7a2b09bbf8
Li-Chian Wang <li-chian@d-172-25-45-140.dhcp.virginia.edu>
2018-01-29 17:46:50 -0500

    updated wendys

--- closest_wendys.py
+++ closest_wendys.py
@@ -36,7 +36,7 @@ for line in datafile:
 datafile.close()
 
 print("The closest Wendy's (", closest_wendys, ") is", closest_dist, "miles away.")
-choice = input("Would you like to see it in google maps? (y/n): ")
+choice = input("Would you like to see it in Google Maps? (y/n): ")
 if choice == 'y':
     google_maps_url = google_maps_url.replace(' ', '+')
     webbrowser.open(google_maps_url)
